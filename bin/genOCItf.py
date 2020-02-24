1a6da825c9129de8c8a645877cc43c783ccc10c6
Daniel Pro <daniel.procopio@gmail.com>
2020-02-24 20:39:46 +0100

    Added bin dir and genOCItf.py program to generate tf files

---
+++ bin/genOCItf.py
@@ -0,0 +1,55 @@
+'''
+Created on Feb 24, 2020
+@author: Daniel Procopio
+@summary: This program invokes the class that generates a config file
+          from a jinja2 template and a YAML config file to generate
+          appropriate Terraform files for OCI
+@version: 1.0
+'''
+
+import argparse
+from jinja2cfg import config
+import logging
+import os
+import sys
+
+
+def generateconfig(args):
+    for file in os.listdir(args.input_tf_templates_dir):
+        if file.endswith(".tf"):
+            newconfig = config.Cfg(args.input_yaml,
+                                   os.path.join(args.input_tf_templates_dir,
+                                                file) ,
+                                   os.path.join(args.output_tf_dir,
+                                                file))
+            newconfig.generate()
+            logger.info(newconfig.getGeneratedCfg())
+            logger.info("Done")
+
+        
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    subparsers = parser.add_subparsers()
+
+    logging.basicConfig(level=logging.DEBUG,
+                        format='%(asctime)s [%(name)-9s] [%(levelname)-8s]'
+                               '- %(message)s',
+                        datefmt='%Y-%m-%d %H:%M')
+    logger = logging.getLogger('genOCItfcli')
+
+    main_parser = subparsers.add_parser('gentf')
+    main_parser.add_argument('input_yaml', help='YAML file contaning the'
+                                                ' configuraton data')
+    main_parser.add_argument('input_tf_templates_dir', help='Jinja2 terraform '
+                             'template directory')
+    main_parser.add_argument('output_tf_dir', help='Generated Terraform '
+                             'directory',
+                             default='')
+    main_parser.set_defaults(func=generateconfig)
+
+    args = parser.parse_args()
+    if len(sys.argv) > 1:
+        args.func(args)
+    else:
+        logger.critical("Missing arguments")
+        main_parser.print_help()
