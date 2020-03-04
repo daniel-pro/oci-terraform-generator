918165830e18c546bbd5b2804f15124f9b3e6174
Daniel Pro <daniel.procopio@gmail.com>
2020-03-04 16:16:28 +0100

    Fixed issue5: Updated templates/ipsecs.tf to reflect OCI new features

--- templates/ipsecs.tf
+++ templates/ipsecs.tf
@@ -4,7 +4,9 @@
 
 resource "oci_core_ipsec" "{{ item.name }}" {
     compartment_id = "${oci_identity_compartment.{{ item.compartment_name }}.id}"
+{% if item.name is defined %}
     display_name = "{{ item.name }}"
+{% endif %}
     cpe_id = "${oci_core_cpe.{{ item.cpe_name }}.id}"
     drg_id = "${oci_core_drg.{{ item.drg_name }}.id}" 
     static_routes = [
@@ -12,12 +14,17 @@ resource "oci_core_ipsec" "{{ item.name }}" {
                      "{{ item_ipsec_phase2_networks }}",
 {% endfor %}
     ]
-{% if item.defined_tags is defined %}
-    defined_tags = { {{ item.defined_tags }} }
+
+{% if item.cpe_local_identifier is defined %}
+    cpe_local_identifier = { {{ item.cpe_local_identifier }} }
 {% endif %}
 
-{% if item.dns_label is defined %}
-    dns_label = "{{ item.dns_label }}"
+{% if item.cpe_local_identifier_type is defined %}
+    cpe_local_identifier_type = { {{ item.cpe_local_identifier_type }} }
+{% endif %}
+
+{% if item.defined_tags is defined %}
+    defined_tags = { {{ item.defined_tags }} }
 {% endif %}
 
 {% if item.freeform_tags is defined %}
