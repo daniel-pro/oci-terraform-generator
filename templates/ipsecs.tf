dc8053a09f383b10003c1045849b51a3283e92c3
Daniel Pro <daniel.procopio@gmail.com>
2021-02-24 14:08:57 +0100

    Added some missing ipsec features.

--- templates/ipsecs.tf
+++ templates/ipsecs.tf
@@ -9,6 +9,7 @@ resource "oci_core_ipsec" "{{ item.name }}" {
 {% endif %}
     cpe_id = oci_core_cpe.{{ item.cpe_name }}.id
     drg_id = oci_core_drg.{{ item.drg_name }}.id 
+
     static_routes = [
 {% for item_ipsec_phase2_networks in item.ipsec_phase2_networks %}
                      "{{ item_ipsec_phase2_networks }}",
@@ -16,11 +17,11 @@ resource "oci_core_ipsec" "{{ item.name }}" {
     ]
 
 {% if item.cpe_local_identifier is defined %}
-    cpe_local_identifier = { {{ item.cpe_local_identifier }} }
+    cpe_local_identifier = "{{ item.cpe_local_identifier }}"
 {% endif %}
 
 {% if item.cpe_local_identifier_type is defined %}
-    cpe_local_identifier_type = { {{ item.cpe_local_identifier_type }} }
+    cpe_local_identifier_type = "{{ item.cpe_local_identifier_type }}"
 {% endif %}
 
 {% if item.defined_tags is defined %}
@@ -32,5 +33,76 @@ resource "oci_core_ipsec" "{{ item.name }}" {
 {% endif %}
 
 }
+
+data "oci_core_ipsec_connection_tunnels" "{{ item.name }}-tunnels" {
+  ipsec_id = oci_core_ipsec.{{ item.name }}.id
+}
+
+resource "oci_core_ipsec_connection_tunnel_management" "{{ item.name }}-tunnel1-mgmt" {
+    #Required
+    ipsec_id = oci_core_ipsec.{{ item.name }}.id
+    tunnel_id = data.oci_core_ipsec_connection_tunnels.{{ item.name }}-tunnels.ip_sec_connection_tunnels[0].id
+    routing = "{{ item.routing }}"
+    #Optional
+{% if item.bgp_session_info is defined %}
+    bgp_session_info {
+        #Optional
+        customer_bgp_asn = var.ip_sec_connection_tunnel_management_bgp_session_info_customer_bgp_asn
+        customer_interface_ip = var.ip_sec_connection_tunnel_management_bgp_session_info_customer_interface_ip
+        oracle_interface_ip = var.ip_sec_connection_tunnel_management_bgp_session_info_oracle_interface_ip
+    }
+{% endif %}
+    display_name = "{{ item.name }}-tunnel1"
+
+{% if item.encryption_domain_config is defined %}
+    encryption_domain_config {
+        #Optional
+        cpe_traffic_selector = var.ip_sec_connection_tunnel_management_encryption_domain_config_cpe_traffic_selector
+        oracle_traffic_selector = var.ip_sec_connection_tunnel_management_encryption_domain_config_oracle_traffic_selector
+    }
+{% endif %}
+
+{% if item.shared_secret is defined %}
+    shared_secret = "{{ item.shared_secret }}"
+{% endif %}
+
+{% if item.ike_version is defined %}
+    ike_version = "{{ item.ike_version }}"
+{% endif %}
+}
+
+resource "oci_core_ipsec_connection_tunnel_management" "{{ item.name }}-tunnel2-mgmt" {
+    #Required
+    ipsec_id = oci_core_ipsec.{{ item.name }}.id
+    tunnel_id = data.oci_core_ipsec_connection_tunnels.{{ item.name }}-tunnels.ip_sec_connection_tunnels[1].id
+    routing = "{{ item.routing }}"
+    #Optional
+{% if item.bgp_session_info is defined %}
+    bgp_session_info {
+        #Optional
+        customer_bgp_asn = var.ip_sec_connection_tunnel_management_bgp_session_info_customer_bgp_asn
+        customer_interface_ip = var.ip_sec_connection_tunnel_management_bgp_session_info_customer_interface_ip
+        oracle_interface_ip = var.ip_sec_connection_tunnel_management_bgp_session_info_oracle_interface_ip
+    }
+{% endif %}
+    display_name = "{{ item.name }}-tunnel2"
+
+{% if item.encryption_domain_config is defined %}
+    encryption_domain_config {
+        #Optional
+        cpe_traffic_selector = var.ip_sec_connection_tunnel_management_encryption_domain_config_cpe_traffic_selector
+        oracle_traffic_selector = var.ip_sec_connection_tunnel_management_encryption_domain_config_oracle_traffic_selector
+    }
+{% endif %}
+
+{% if item.shared_secret is defined %}
+    shared_secret = "{{ item.shared_secret }}"
+{% endif %}
+
+{% if item.ike_version is defined %}
+    ike_version = "{{ item.ike_version }}"
+{% endif %}
+}
+
 {% endfor %}
 {%- endif %}
