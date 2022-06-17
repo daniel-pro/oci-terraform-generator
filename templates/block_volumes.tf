e5f95852c39a09bec27c25effd16366d739660e3
Daniel Pro <daniel.procopio@gmail.com>
2022-06-17 12:23:27 +0200

    DP - Fixed syntax

--- templates/block_volumes.tf
+++ templates/block_volumes.tf
@@ -2,26 +2,26 @@
 
 {% for item in oci_block_volumes %}
 resource "oci_core_volume" "{{ item.name }}" {
-    compartment_id = "oci_identity_compartment.{{ item.compartment_name }}.id"
+    compartment_id = oci_identity_compartment.{{ item.compartment_name }}.id
     availability_domain = "{{ item.availability_domain }}" 
     display_name = "{{ item.name }}"
     size_in_gbs  = "{{ item.size_in_gbs }}"
 
 {% if item.backup_policy_id is defined %}
-    backup_policy_id = "$oci_core_backup_policy.{{ item.backup_policy_name }}.id"
+    backup_policy_id = oci_core_backup_policy.{{ item.backup_policy_name }}.id
 {% endif %}
 
 {% if (item.source_details_id is defined) and (item.source_details_type is defined) %}
     source_details {
-        id = "oci_core_volume.{{ item.source_volume_name }}.id"
+        id = oci_core_volume.{{ item.source_volume_name }}.id
         type = "{{ item.source_volume_type }}"
     }
-    backup_policy_id = "$oci_core_backup_policy.{{ item.backup_policy_name }}.id"
+    backup_policy_id = oci_core_backup_policy.{{ item.backup_policy_name }}.id
 {% endif %}
 
 
 {% if item.kms_key_id is defined %}
-    backup_policy_id = "$oci_core_kms_key.{{ item.kms_key_name }}.id"
+    backup_policy_id = oci_core_kms_key.{{ item.kms_key_name }}.id
 {% endif %}
 
 {% if item.defined_tags is defined %}
