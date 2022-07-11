23af6f8526a14457396cf995dbde619a5f88ae67
Daniel Pro <daniel.procopio@gmail.com>
2022-07-11 18:33:18 +0200

    DP - Added new features to DRGs

--- templates/drgs.tf
+++ templates/drgs.tf
@@ -20,11 +20,42 @@ resource "oci_core_drg" "{{ item.name }}" {
 
 }
 
-resource "oci_core_drg_attachment" "{{ item.name }}-{{ item.attach_to_vcn }}" {
+{% for drg_attachment in item.drg_attachments %}
+resource "oci_core_drg_attachment" "{{ item.name }}-{{ drg_attachment.attach_to_vcn }}" {
+    #Required
     drg_id = oci_core_drg.{{ item.name }}.id
-    vcn_id = oci_core_vcn.{{ item.attach_to_vcn }}.id
-    display_name = "{{ item.attachment_name }}"
+
+    #Optional
+{% if drg_attachment.defined_tags is defined %}
+    defined_tags = { {{ drg_attachment.defined_tags }} }
+{% endif %}
+
+    display_name = "{{ item.name }}-{{ drg_attachment.attach_to_vcn }}"
+
+{% if drg_attachment.drg_route_table_id is defined %}
+    drg_route_table_id = "{{ drg_attachment.drg_route_table_id }}"
+{% endif %}
+{% if drg_attachment.freeform_tags is defined %}
+    freeform_tags = { {{ drg_attachment.freeform_tags }} }
+{% endif %}
+
+
+    network_details {
+        #Required
+        id = oci_core_vcn.{{ drg_attachment.attach_to_vcn }}.id
+        type = "{{ drg_attachment.network_details.type }}"
+
+        #Optional
+{% if drg_attachment.network_details.route_table_name is defined %}
+        route_table_id = oci_core_route_table.{{ route_table_name }}.id
+{% endif %}
+{% if drg_attachment.network_details.vcn_route_type is defined %}
+        vcn_route_type ="{{ drg_attachment.network_details.vcn_route_type }}"
+{% endif %}
+    }
+
 }
+{% endfor %}
 
 {% endfor %}
 {%- endif %}
